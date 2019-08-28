bfc616d4b4ed9c45b620b668ac20d5b198fae27e
Joelle Helm <joellenhelm@gmail.com>
2019-08-28 15:00:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,5 +14,5 @@ def reverse_each_word(string)
   arr = string.split(" ")
   reversed = []
   arr.collect { |word| reversed << word.reverse }
-  return arr.join(" ")
+  return reversed.join(" ")
 end
